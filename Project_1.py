eace1e660dc390c2ca9650286c80fd348fa81427
1819210046 <anandgupta9760@gmail.com>
2021-06-25 15:04:31 +0530

    updated file

--- Project_1.py
+++ Project_1.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import Project_2
 
 #Read the image
 Image = "D:\\New folder\\OMR\\testCase\\test3.jpg"
@@ -20,3 +21,16 @@ grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#Gray Image
 blurImg = cv2.GaussianBlur(grayImg, (5, 5), 1)#Blur Gray Image
 cannyImg = cv2.Canny(blurImg, 10, 50)#Canny Image
 
+cont, hierarchy = cv2.findContours(
+    cannyImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)# Find all Contours
+rectCon = Project_2.rectCont(cont) #Filter rectangles from Countours
+bigCont = Project_2.getCornerPoints(rectCon[0])#Corner points of biggest rectangle(For OMR box)
+gradeScore = Project_2.getCornerPoints(rectCon[1])#Corner points of secnod biggest rectangle(For grade box)
+bigCont = Project_2.reorder(bigCont)
+gradeScore = Project_2.reorder(gradeScore)
+
+pt1 = np.float32(bigCont)
+pt2 = np.float32(
+    [[0, 0], [ImgWidth, 0], [0, Imgheight], [ImgWidth, Imgheight]])
+matrix = cv2.getPerspectiveTransform(pt1, pt2)
+imgCol = cv2.warpPerspective(img, matrix, (ImgWidth, Imgheight))
---
+++ Project_2.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+
+def rectCont(cont):#Check if given contour is rectangle
+    rectCon = []
+    for i in cont:
+        area = cv2.contourArea(i)
+        if area > 50:
+            perimeter = cv2.arcLength(i, True)
+            sides = cv2.approxPolyDP(i, 0.02 * perimeter, True)
+            if len(sides) == 4:
+                rectCon.append(i)
+    rectCon = sorted(rectCon, key=cv2.contourArea, reverse=True)
+    return rectCon
+
+
+def getCornerPoints(cont):#Find the corners of given the rectangle
+    perimeter = cv2.arcLength(cont, True)
+    sides = cv2.approxPolyDP(cont, 0.02 * perimeter, True)
+    return sides
+
+
+def reorder(points):#Arrange all the points of rectangle 
+    points = points.reshape((4, 2))
+    newPoints = np.zeros((4, 1, 2), np.int32)
+    add = points.sum(1)
+    newPoints[0] = points[np.argmin(add)]
+    newPoints[3] = points[np.argmax(add)]
+    diff = np.diff(points, axis=1)
+    newPoints[1] = points[np.argmin(diff)]
+    newPoints[2] = points[np.argmax(diff)]
+    return newPoints
