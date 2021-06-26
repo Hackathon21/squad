64c24adde2b1f660e98f29ba25f56272f4d29eb7
Harsh Gupta <hg.harsh28@gmail.com>
2021-06-26 10:41:13 +0530

    Update Project_1.py

--- Project_1.py
+++ Project_1.py
@@ -3,7 +3,7 @@ import numpy as np
 import Project_2
 
 #Read the image
-Image = "D:\\New folder\\OMR\\testCase\\test3.jpg"
+Image = "D:\\New folder\\OMR\\testCase\\original.jpg"
 ImgWidth = 700
 Imgheight = 700
 questions = 5
@@ -41,15 +41,14 @@ matrixG = cv2.getPerspectiveTransform(ptG1, ptG2)
 imgSD = cv2.warpPerspective(img, matrixG, (325, 150))
 
 
-imgWG = cv2.cvtColor(imgCol, cv2.COLOR_BGR2GRAY) # Converting image to grayscale
-imgThreshold = cv2.threshold(imgWG, 170, 255, cv2.THRESH_BINARY_INV)[1] 
-boxes = Project_2.splitBoxes(imgThreshold, questions, options) # Split the image in boxes
+imgWG = cv2.cvtColor(imgCol, cv2.COLOR_BGR2GRAY)
+imgThreshold = cv2.threshold(imgWG, 170, 255, cv2.THRESH_BINARY_INV)[1]
+boxes = Project_2.splitBoxes(imgThreshold, questions, options)
 
 rowCount = 0
 colCount = 0
 pixelValue = np.zeros((questions, options))
 
-#Counting and storing no. of pixels in each box
 for val in boxes:
     netPixel = cv2.countNonZero(val)
     pixelValue[rowCount][colCount] = netPixel
@@ -58,7 +57,6 @@ for val in boxes:
         colCount = 0
         rowCount += 1
 
-#Finding the box with max pixel value in each row
 index = []
 for x in range(0, questions):
     arr = pixelValue[x]
@@ -68,3 +66,31 @@ for x in range(0, questions):
         index.append(indexVal[0][0])
     else:
         index.append(-1)
+        
+scoring = []
+for x in range(0, questions):
+    if solu[x] == index[x]:
+        scoring.append(1)
+    else:
+        scoring.append(0)
+score = (sum(scoring)/questions)*100
+resultImg = imgCol.copy()
+resultImg = Project_2.showAnswers(
+    resultImg, index, scoring, solu, questions, options)
+rawImg = np.zeros_like(imgCol)
+rawImg = Project_2.showAnswers(
+    rawImg, index, scoring, solu, questions, options)
+invertedMatrix = cv2.getPerspectiveTransform(pt2, pt1)
+imgInvWarp = cv2.warpPerspective(rawImg, invertedMatrix, (ImgWidth, Imgheight))
+
+rawScoreImg = np.zeros_like(imgSD)
+cv2.putText(rawScoreImg, str(int(score))+"%", (50, 100),
+            cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 255), 3)
+revMatrix = cv2.getPerspectiveTransform(ptG2, ptG1)
+revImgGD = cv2.warpPerspective(rawScoreImg, revMatrix, (ImgWidth, Imgheight))
+
+finalImg = cv2.addWeighted(finalImg, 1, imgInvWarp, 1, 0)
+finalImg = cv2.addWeighted(finalImg, 1, revImgGD, 1, 0)
+
+cv2.imshow("Final Image", finalImg)
+cv2.waitKey(0)
