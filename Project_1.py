a31d4a976580a7ab807c8295501fd2af00bf244f
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2021-06-26 12:51:47 +0530

    Comments added

--- Project_1.py
+++ Project_1.py
@@ -49,6 +49,7 @@ rowCount = 0
 colCount = 0
 pixelValue = np.zeros((questions, options))
 
+#Counting and storing no. of pixels in each box
 for val in boxes:
     netPixel = cv2.countNonZero(val)
     pixelValue[rowCount][colCount] = netPixel
@@ -57,6 +58,7 @@ for val in boxes:
         colCount = 0
         rowCount += 1
 
+#Finding the box with max pixel value in each row
 index = []
 for x in range(0, questions):
     arr = pixelValue[x]
@@ -66,17 +68,18 @@ for x in range(0, questions):
         index.append(indexVal[0][0])
     else:
         index.append(-1)
-        
+
+#Calculating the score for each question
 scoring = []
 for x in range(0, questions):
     if solu[x] == index[x]:
-        scoring.append(1)
+        scoring.append(1) #If answer is correct append 1
     else:
-        scoring.append(0)
+        scoring.append(0) #If answer is wrong append 0
 score = (sum(scoring)/questions)*100
 resultImg = imgCol.copy()
 resultImg = Project_2.showAnswers(
-    resultImg, index, scoring, solu, questions, options)
+    resultImg, index, scoring, solu, questions, options) 
 rawImg = np.zeros_like(imgCol)
 rawImg = Project_2.showAnswers(
     rawImg, index, scoring, solu, questions, options)
@@ -85,15 +88,15 @@ imgInvWarp = cv2.warpPerspective(rawImg, invertedMatrix, (ImgWidth, Imgheight))
 
 rawScoreImg = np.zeros_like(imgSD)
 cv2.putText(rawScoreImg, str(int(score))+"%", (50, 100),
-            cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 255), 3)
+            cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 255), 3) #Adding the score in score box
 revMatrix = cv2.getPerspectiveTransform(ptG2, ptG1)
 revImgGD = cv2.warpPerspective(rawScoreImg, revMatrix, (ImgWidth, Imgheight))
 
 finalImg = cv2.addWeighted(finalImg, 1, imgInvWarp, 1, 0)
 finalImg = cv2.addWeighted(finalImg, 1, revImgGD, 1, 0)
 
-# printing the final output
+#Printing the final output after evaluation
 cv2.imshow("Final Image", finalImg)
 
-#wait 0 means manually closing thw window
+#Wait 0 means manually closing the window
 cv2.waitKey(0)
